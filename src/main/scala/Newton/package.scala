6021db72378c44079fc70de92982cbeff3a37e1b
Ronald Vera <ronalddavidverameneses@gmail.com>
2026-05-10 20:34:05 -0500

    Se termino el punto 1,3 Evaluando expresiones

--- src/main/scala/Newton/package.scala
+++ src/main/scala/Newton/package.scala
@@ -1,14 +1,21 @@
 package object Newton {
 
-    trait Expr 
-    case class Numero (d:Double) extends Expr
-    case class Atomo (x:Char) extends Expr
-    case class Suma (e1:Expr, e2:Expr) extends Expr
-    case class Prod (e1:Expr, e2:Expr) extends Expr
-    case class Resta (e1:Expr, e2:Expr) extends Expr
-    case class Div (e1:Expr, e2:Expr) extends Expr
-    case class Expo (e1:Expr, e2:Expr) extends Expr
-    case class Logaritmo (e1:Expr) extends Expr
+    trait Expr
+        case class Numero(d: Double) extends Expr
+
+        case class Atomo(x: Char) extends Expr
+
+        case class Suma(e1: Expr, e2: Expr) extends Expr
+
+        case class Prod(e1: Expr, e2: Expr) extends Expr
+
+        case class Resta(e1: Expr, e2: Expr) extends Expr
+
+        case class Div(e1: Expr, e2: Expr) extends Expr
+
+        case class Expo(e1: Expr, e2: Expr) extends Expr
+
+        case class Logaritmo(e1: Expr) extends Expr
 
     def mostrar (e:Expr): String = e match {
 
@@ -43,5 +50,20 @@ package object Newton {
         )
 
     }
-    
+
+    def evaluar ( f:Expr , a :Atomo , v:Double ) : Double =  f match{
+        case  Numero(d) => d
+        case  Atomo(x) => v
+        case Suma(e1,e2) => evaluar(e1,a,v) + evaluar(e2,a, v)
+        case Resta(e1,e2) => evaluar(e1,a,v) - evaluar(e2,a, v)
+        case Prod(e1,e2) => evaluar(e1,a,v) * evaluar(e2,a, v)
+        case Div(e1,e2) => evaluar(e1,a,v) / evaluar(e2,a, v)
+        case Expo(e1,e2) => math.pow(evaluar(e1,a,v), evaluar(e2,a, v))
+        case Logaritmo(e1) =>  Math.log(evaluar(e1, a, v))
+
+
+    }
+
+
+
 }
